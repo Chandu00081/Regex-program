7000069c884da3ed40a7e8bcb329cbf2847b8ed1
chandu00081 <chandu00081@gmail.com>
2023-02-16 13:41:22 +0530

    Validating first name

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regexp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Regex  Statement Using REGEX");
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string fname = Console.ReadLine();
+            userValidation.ValidationFirstName(fname);
+        }
+    }
+}
---
+++ UserValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regexp
+{
+    internal class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationFirstName(string fname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(fname);
+            Console.WriteLine(result);
+        }
+    } 
+}
